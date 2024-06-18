fa3a98a89691ac9b26281a44c4a649e5f013e1b0
Hayden32D <HDouglas@method-automation.com>
2024-06-18 15:41:31 -0400

    Update masterFile.py

--- Git repos/ManyBuild Web/user/masterFile.py
+++ Git repos/ManyBuild Web/user/masterFile.py
@@ -1,9 +1,14 @@
 import userForHomeComputer
 import profileTests
+import jigxTest
 
 
 # Run the tests for the user GETs
-# userForHomeComputer.userTests() #uncomment to run
+userForHomeComputer.userTests() #uncomment to run
 
 # Run profile tests
-profileTests.runProfile()
+profileTests.runProfile() #uncomment to run
+
+#run jigx get test
+jigxTest.runJigTest() #uncomment to run
+
